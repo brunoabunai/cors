e6ba430d3db645129020843c6fb5cabab2252bb7
Carlos Calixto <carlosedu.calixto2@gmail.com>
2021-07-26 21:28:48 -0300

    change color in svg of back button in unplugged

--- src/pages/Unplugged/unplugged.php
+++ src/pages/Unplugged/unplugged.php
@@ -21,7 +21,7 @@
 
             <a href="./index.php?p=landing" class="btn-loginBack opacty-button">
                   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M7.83 11L11.41 7.41L10 6L4 12L10 18L11.41 16.59L7.83 13H20V11H7.83Z" fill="var(--null)"></path>
+                        <path d="M7.83 11L11.41 7.41L10 6L4 12L10 18L11.41 16.59L7.83 13H20V11H7.83Z" fill="var(--text-color)"></path>
                   </svg>
                   Voltar Para o Início</a>
       </div>
