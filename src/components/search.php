73b6a0f16e4d902fc96407f0c5c4e5715862ee33
brunoabunai <bruno.afonsorr@gmail.com>
2021-08-03 20:07:10 -0300

    criate backand (edit.php)

--- src/components/search.php
+++ src/components/search.php
@@ -1,3 +1,57 @@
 <?php
+include_once './connection.php';
+
+  // @session_start();
+  $limit = 5;
+  $action = $_POST['action'];
   
-?>
+  if ($_POST['actualPage'] > 1) {
+    $start = (($_POST['actualPage']-1) * $limit);
+    $page = $_POST['actualPage'];
+  } else {
+    $start = 0;
+  }
+
+  //Choose Primary funcitons from component
+  $query = " Select * FROM users ";
+  if ($action != '') {
+    $query .= '
+      WHERE use_name LIKE "%'.str_replace(' ', '%', $action).'%"
+    ';
+  }
+$query .= ' ORDER BY use_name DESC ';
+
+$queryLimit = $query.'LIMIT '.$start.', '.$limit;
+
+$querys = $conn->query($query) or die ($conn->error);
+$totalData = $querys->num_rows;
+
+$limiteQueryPerPages = $conn->query($queryLimit) or die ($conn->error);
+$data = $limiteQueryPerPages->fetch_assoc();
+
+$output = '
+  <div class="container">
+  <label>Total de Registros - '.$totalData.'</label>
+';
+
+  if ($totalData > 0) {
+    foreach ($limiteQueryPerPages as $data) {
+      $output .= '
+        <div class="resultSearch">
+          <img src="'.$data["use_avatar"].'" />
+          <label>'.$data["use_name"].'</label>
+        </div>
+      ';
+      // $_SESSION['editId'] = $data['use_idPk'];
+    }
+  }
+
+$output .= '
+  </div>
+';
+
+if (strlen($action) == 0) {
+  $output = '<h1>Not found</h1>';
+}
+echo $output;
+//Edita a linha 33 e a linha 40
--- src/utils/boxes.js
+++ src/utils/boxes.js
@@ -1,54 +1,54 @@
 var globalStateBox = [false, false];
 
 function CreateBoxInit(op) {
-    return (conditionBox()) ? makeBox(op) : removeBox(op)
+  return (conditionBox()) ? makeBox(op) : removeBox(op)
 }
 
 function makeBox(op) {
-    const box = constructorBox(op);
-    addInPage(box);
-    changeStateBox(op);
+  const box = constructorBox(op);
+  addInPage(box);
+  changeStateBox(op);
 }
 
 function removeBox(op) {
-    const box = [localizeID('box'), localizeID('boxInput')];
-    box[op].remove();
-    changeStateBox(op);
+  const box = [localizeID('box'), localizeID('boxInput')];
+  box[op].remove();
+  changeStateBox(op);
 }
 
 function localizeID(item) {
-    return document.getElementById(item);
+  return document.getElementById(item);
 }
 
 function changeStateBox(op) {
-    globalStateBox[op] = !globalStateBox[op];
+  globalStateBox[op] = !globalStateBox[op];
 }
 
 function conditionBox() {
-    return (!globalStateBox[0] && !globalStateBox[1]);
+  return (!globalStateBox[0] && !globalStateBox[1]);
 }
 
 function addInPage(content) {
-    document.querySelector('body').innerHTML += content;
+  document.querySelector('body').innerHTML += content;
 }
 
 function constructorBox(op) {
-    let box = { name: "", content: "" };
-    switch (op) {
-        case 0:
-            box.name = "box of config";
-            box.content =
-                `
+  let box = { name: "", content: "" };
+  switch (op) {
+    case 0:
+      box.name = "box of config";
+      box.content =
+        `
                 <div id='box'>
                     <a class="btn-account opacity-button" href="oi.html"> minha conta </a>
                     <a class="btn-leave opacity-button" onclick="logOff()" href="#"> sair </a>
                 </div>
             `;
-            break;
-        case 1:
-            box.name = "box of search";
-            box.content =
-                `
+      break;
+    case 1:
+      box.name = "box of search";
+      box.content =
+        `
                 <div id='boxInput'>
                     <div>
                         <svg class="opacty-button" onclick="removeBox(1)" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -57,15 +57,14 @@ function constructorBox(op) {
     
                     </div>
                     <h3>Digite o Nome:</h3>
-                    <input id="search" type='text'/>
+                    <input id="searchInput2" type='text'/>
                     <button class="opacty-button" onclick="SearchInit(1)"> Buscar </button>
                 </div>
             `;
-            break;
-        default:
-            box.name = "null";
-            box.content = "";
-    }
-    return box.content;
+      break;
+    default:
+      box.name = "null";
+      box.content = "";
+  }
+  return box.content;
 }
-
