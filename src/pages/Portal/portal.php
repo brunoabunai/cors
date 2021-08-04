9b1c2719f79d605298dc740dde32a6aaa1adf7c8
Carlos Calixto <carlosedu.calixto2@gmail.com>
2021-08-03 21:54:30 -0300

    edit portal page with topics

--- src/pages/Portal/portal.php
+++ src/pages/Portal/portal.php
@@ -12,53 +12,29 @@
 </head>
 
 <body>
-  <div class="landing">
-    <h2 class="title">
-      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M16 2C17.1046 2 18 2.89543 18 4L4 4L4 15.1765C2.89543 15.1765 2 14.281 2 13.1765V4C2 2.89543 2.89543 2 4 2H16Z" fill="#5438DC"></path>
-        <path d="M14 22L11.3333 19.1765H8C6.89543 19.1765 6 18.281 6 17.1765V8C6 6.89543 6.89543 6 8 6H20C21.1046 6 22 6.89543 22 8V17.1765C22 18.281 21.1046 19.1765 20 19.1765H16.6667L14 22ZM15.8046 17.1765L20 17.1765V8L8 8V17.1765H12.1954L14 19.0872L15.8046 17.1765Z" fill="#5438DC"></path>
-      </svg>
+  <div class="portal-page">
+    <div class="news">
+        Ultimas Publicações
 
-      <b class="brand">
-        CORS
-      </b>
+        <div id="last_news">
 
-      - Informações Sobre o Novo Corona Vírus
-    </h2>
-
-    <span class="subtitle">
-      Dados confiáveis para vidas seguras. Entregando usabilidade e informação de um jeito nunca visto.
-    </span>
+        </div>
+    </div>
+    <div class="btn-options">
+      <a href="#">Administração</a>
+      <a href="#">Novas Postagens</a>
+      <a href="#">Variantes</a>
+      <a href="#">Vacinas</a>
+      <a href="#">Números de Casos e Óbitos</a>
+      <a href="#">Prevenções</a>
+    </div>
   </div>
 
-  <div class="main">
-    <a class="btn-begin opacty-button" href="./index.php?p=portal">Começar Agora</a>
-    <a class="anc-admin opacty-button" onclick="logedOrNoLoged()" href>admin<small>></small></a>
-  </div>
-
-  <div id="redirect">
-
-  </div>
-
-  <script src="./node_modules/jquery/dist/jquery.js"></script>
 </body>
 
 </html>
 
-<script>
-  const logedOrNoLoged = () => {
-    $.ajax({
-      type: "POST",
-      url: './src/components/geralCommands.php',
-      data: {
-        action: 'logedOrNoLoged'
-      },
-      success: function(html) {
-        $('#redirect').html(html);
-      },
-      error: function(html) {
-        location.href = '../../404.php';
-      }
-    })
-  }
-</script>
+
+<!-- 
+      
+-->
