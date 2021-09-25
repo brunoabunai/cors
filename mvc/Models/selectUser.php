bea1ca48a24c5d337a7f316a60643be2a2ef26f4
Eduardo Calixto <71821586+educalixto@users.noreply.github.com>
2021-09-24 21:22:09 -0300

    create selectUser front-end and make changes in home.php

--- mvc/Models/selectUser.php
+++ mvc/Models/selectUser.php
@@ -67,22 +67,36 @@ require_once ('connection.php');
       $row = $limitQueryPages->fetch_assoc();
 
       $output = '
-      <label>Total de Registros - '.$totalData.'</label>
-      <table class="">
-        <tr>
-          <th>Creator</th>
-          <th>Name</th>
-          <th>Image</th>
+      <!-- <label>Total de Registros - '.$totalData.'</label> -->
+      <table class="search-users">
+        <tr class="head-users">
+          <th>Avatar</th>
+          <th>Nome de usuário</th>
+          <th class="id">ID</th>
+          <th class="options">Opções</th>
         </tr>
       ';
 
       if ($totalData > 0) {
         foreach ($limitQueryPages as $row) {
           $output .= '
-          <tr>
-            <td>'.$row["typ_IdFk"].'</td>
+          <tr class="column-user">
+            <td><img src="'.$row["use_avatar"].'"/></td>
             <td>'.$row["use_name"].'</td>
-            <td>'.$row["use_avatar"].'</td>
+            <td class="id">'.$row["typ_IdFk"].'</td>
+            <td class="options-user options">
+              <button class="btn-delete-user">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M17 22H7C5.89543 22 5 21.1046 5 20V7H3V5H7V4C7 2.89543 7.89543 2 9 2H15C16.1046 2 17 2.89543 17 4V5H21V7H19V20C19 21.1046 18.1046 22 17 22ZM7 7V20H17V7H7ZM9 4V5H15V4H9Z" fill="var(--danger)"></path>
+              </svg>
+              <button> 
+              <button class="btn-edit-user">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4.41999 20.579C4.13948 20.5785 3.87206 20.4603 3.68299 20.253C3.49044 20.0475 3.39476 19.7695 3.41999 19.489L3.66499 16.795L14.983 5.48103L18.52 9.01703L7.20499 20.33L4.51099 20.575C4.47999 20.578 4.44899 20.579 4.41999 20.579ZM19.226 8.31003L15.69 4.77403L17.811 2.65303C17.9986 2.46525 18.2531 2.35974 18.5185 2.35974C18.7839 2.35974 19.0384 2.46525 19.226 2.65303L21.347 4.77403C21.5348 4.9616 21.6403 5.21612 21.6403 5.48153C21.6403 5.74694 21.5348 6.00146 21.347 6.18903L19.227 8.30903L19.226 8.31003Z" fill="var(--alert)"></path>
+                </svg>
+              
+              <button> 
+            </td>
           </tr>
           ';
         }
@@ -91,109 +105,109 @@ require_once ('connection.php');
         <tr>
           <td colspan="4" align="center">No Data Found</td>
         </tr>
+        </table>
         ';
       }
 
       /** Final das configurações básicas */
 
       /** -------------------------------- */
-      $totalLinks = ceil($totalData / $limit);
-      $pageArray = array();
-      $previewsLink = '';
-      $nextLink = '';
-      $pageLink = '';
+      // $totalLinks = ceil($totalData / $limit);
+      // $pageArray = array();
+      // $previewsLink = '';
+      // $nextLink = '';
+      // $pageLink = '';
 
-      /** Iniciao da paginação */
-      if($totalLinks > 4){
-        if($pages < 5){
-          for($count = 1; $count <= 5; $count++){
-            $pageArray[] = $count;
-          }
-          $pageArray[] = '...';
-          $pageArray[] = $totalLinks;
+      // /** Iniciao da paginação */
+      // if($totalLinks > 4){
+      //   if($pages < 5){
+      //     for($count = 1; $count <= 5; $count++){
+      //       $pageArray[] = $count;
+      //     }
+      //     $pageArray[] = '...';
+      //     $pageArray[] = $totalLinks;
 
-        }else{
-          $endLimit = $totalLinks - 5;
-          if($pages > $endLimit){
-            $pageArray[] = 1;
-            $pageArray[] = '...';
-            for($count = $endLimit; $count <= $totalLinks; $count++){
-              $pageArray[] = $count;
-            }
+      //   }else{
+      //     $endLimit = $totalLinks - 5;
+      //     if($pages > $endLimit){
+      //       $pageArray[] = 1;
+      //       $pageArray[] = '...';
+      //       for($count = $endLimit; $count <= $totalLinks; $count++){
+      //         $pageArray[] = $count;
+      //       }
 
-          }else{
-            $pageArray[] = 1;
-            $pageArray[] = '...';
-            for($count = $pages - 1; $count <= $pages + 1; $count++){
-              $pageArray[] = $count;
-            }
-            $pageArray[] = '...';
-            $pageArray[] = $totalLinks;
+      //     }else{
+      //       $pageArray[] = 1;
+      //       $pageArray[] = '...';
+      //       for($count = $pages - 1; $count <= $pages + 1; $count++){
+      //         $pageArray[] = $count;
+      //       }
+      //       $pageArray[] = '...';
+      //       $pageArray[] = $totalLinks;
 
-          }
-        }
+      //     }
+      //   }
 
-      }else{
-        for($count = 1; $count <= $totalLinks; $count++){
-          $pageArray[] = $count;
-        }
+      // }else{
+      //   for($count = 1; $count <= $totalLinks; $count++){
+      //     $pageArray[] = $count;
+      //   }
 
-      }
-      /** Paginação final */
+      // }
+      // /** Paginação final */
 
-      /** --------------------- */
+      // /** --------------------- */
 
-      /** Inicio do controle da paginação */
+      // /** Inicio do controle da paginação */
 
-      for($count = 0; $count < count($pageArray); $count++){
-        if($pages == $pageArray[$count]){
-          $pageLink .= '
-          <li class="">
-            <a class="" href="#">'.$pageArray[$count].'</a>
-          </li>
-          ';
+      // for($count = 0; $count < count($pageArray); $count++){
+      //   if($pages == $pageArray[$count]){
+      //     $pageLink .= '
+          
+      //       <a class="" href="#">'.$pageArray[$count].'</a>
+          
+      //     ';
     
-          $previousId = $pageArray[$count] - 1;
-          if($previousId > 0){
-            $previewsLink = '<li class=""><a class="" href="javascript:void(0)" data-page_number="'.$previousId.'">Previous</a></li>';
-          }else{
-            $previewsLink = '
-            <li class="">
-              <a class="" href="#">Previous</a>
-            </li>
-            ';
-          }
+      //     $previousId = $pageArray[$count] - 1;
+      //     if($previousId > 0){
+      //       $previewsLink = '<a class="" href="javascript:void(0)" data-page_number="'.$previousId.'">Previous</a>';
+      //     }else{
+      //       $previewsLink = '
+            
+      //       <a class="" href="#">Previous</a>
+      //       ';
+      //     }
     
-          $nextId = $pageArray[$count] + 1;
-          if($nextId > $totalLinks){
-            $nextLink = '
-            <li class="page-item disabled">
-              <a class="page-link" href="#">Next</a>
-            </li>
-              ';
-          }else{
-            $nextLink = '<li class="page-item"><a class="page-link" href="javascript:void(0)" data-page_number="'.$nextId.'">Next</a></li>';
-          }
-        }else{
-          if($pageArray[$count] == '...'){
-            $pageLink .= '
-            <li class="page-item disabled">
-                <a class="page-link" href="#">...</a>
-            </li>
-            ';
-          }else{
-            $pageLink .= '
-            <li class=""><a class="" href="javascript:void(0)" data-page_number="'.$pageArray[$count].'">'.$pageArray[$count].'</a></li>
-            ';
-          }
-        }
-      }
+      //     $nextId = $pageArray[$count] + 1;
+      //     if($nextId > $totalLinks){
+      //       $nextLink = '
+            
+      //         <a class="page-link" href="#">Next</a>
+           
+      //         ';
+      //     }else{
+      //       $nextLink = '<a class="page-link" href="javascript:void(0)" data-page_number="'.$nextId.'">Next</a>';
+      //     }
+      //   }else{
+      //     if($pageArray[$count] == '...'){
+      //       $pageLink .= '
+      //       <li class="page-item disabled">
+      //           <a class="page-link" href="#">...</a>
+      //       </li>
+      //       ';
+      //     }else{
+      //       $pageLink .= '
+      //       <li class=""><a class="" href="javascript:void(0)" data-page_number="'.$pageArray[$count].'">'.$pageArray[$count].'</a></li>
+      //       ';
+      //     }
+      //   }
+      // }
 
-      $output .= $previewsLink . $pageLink . $nextLink;
-      $output .= '
-        </>
-      </div>
-      ';
+      // $output .= $previewsLink . $pageLink . $nextLink;
+      // $output .= '
+      //   </>
+      // </div>
+      // ';
 
       echo $output;
     }
--- mvc/Views/selectUser.php
+++ mvc/Views/selectUser.php
@@ -1,58 +1,61 @@
 <!DOCTYPE html>
 <html lang="pt">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.css">
-    <title>Select User</title>
 
-    <style>
-      .col {
-        margin: 1rem;
-      }
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.css">
+  <title>Select User</title>
 
-      .page-link  {
-        height: 100%;
-      }
+  <style>
+    <?php include_once('styles/selectUser.css'); ?>
+  </style>
 
-    </style>
-    
-  </head>
-  <body>
-    <a class="anc-back" href="../menu">
-      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M6 16V13L22 13V11L6 11L6 8L2 12L6 16Z" fill="#343434"></path>
-      </svg>
-    </a>
-    <h1>Users from select</h1>
-    <input type="text" name="searchUser" id="searchInput">
+</head>
+
+<body>
+  <a class="anc-back" href="./menu">
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M6 16V13L22 13V11L6 11L6 8L2 12L6 16Z" fill="#343434"></path>
+    </svg>
+  </a>
+  <div class="selectUser-page">
+
+    <div class="search">
+      <h2>Primeiro Pesquise o Admin</h2>
+      <input type="text" name="searchUser" id="searchInput">
+    </div>
+
 
     <div class="users">
 
-    <?php
+      <?php
       // for($i = 0; $i < count($this->data[0]); $i++) {
-    ?>
+      ?>
 
-        <!-- <a href="<?php echo 'editUser/'.str_replace(" ", "-", $this->data[0][$i]['use_name']); ?>">
+      <!-- <a href="<?php echo 'editUser/' . str_replace(" ", "-", $this->data[0][$i]['use_name']); ?>">
           <div>
-            <h3><?php //echo $this->data[0][$i]['use_name']; ?></h3>
-            <img src="<?php //echo $this->data[0][$i]['use_avatar']; ?>" />
+            <h3><?php //echo $this->data[0][$i]['use_name']; 
+                ?></h3>
+            <img src="<?php //echo $this->data[0][$i]['use_avatar']; 
+                      ?>" />
           </div>
         </a> -->
 
-    <?php
+      <?php
       // }
-    ?>
+      ?>
 
     </div>
+  </div>
+  <script defer src="../../node_modules/jquery/dist/jquery.js"></script>
+</body>
 
-    <script defer src="../../node_modules/jquery/dist/jquery.js"></script>
-  </body>
 </html>
 
 <script defer type="module">
-  function loadDatas(page, query = '') {    
+  function loadDatas(page, query = '') {
     $.ajax({
       type: "POST",
       url: './search', //config url execute
@@ -74,7 +77,7 @@
         console.log(html);
       }
     });
-  
+
   }
 
   $('#searchInput').keyup(function() {
